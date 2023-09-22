0bb2ddea4731032dc5f225b56a425a786abfbdee
Athanasios Spyridon Skandalos <80396487+Xtheta@users.noreply.github.com>
2023-09-22 20:48:58 +0300

    Add files via upload

--- QuadTree.py
+++ QuadTree.py
@@ -1,8 +1,18 @@
+import pandas as pd
+
+
 class Point:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, data=None):
         self.x = x
         self.y = y
+        self.data = []
+        self.data.append(data)
+
+    def __str__(self):
+        return "x is " + str(self.x) + " y is " + str(self.y)
+    def same_as(self, point: 'Point'):
+        return self.x == point.x and self.y == point.y
 
 
 class Rectangle:
@@ -28,7 +38,7 @@ class Rectangle:
         return (self.high.y + self.low.y)/2
 
     def center(self):
-        return Point(self.x_median(), self.y_median())
+        return Point(self.x_median(), self.y_median(),)
 
     def overlaps_point(self, point: Point):
         return self.low.x <= point.x <= self.high.x and self.low.y <= point.y <= self.high.y
@@ -55,7 +65,7 @@ class Node:
                     return False
         return True
 
-    def is_leaf(self) -> bool:
+    def isleaf(self) -> bool:
         return self.is_leaf
 
 
@@ -74,6 +84,7 @@ class QuadTree:
             if node.is_leaf:
                 if node.point is None:
                     node.point = point
+
                     loop_check += 1
                 else:
                     node = self.split_node(node, node.rec, point)
@@ -84,6 +95,12 @@ class QuadTree:
                         node = child_node
 
     def split_node(self, node: Node, rec: Rectangle, point: Point):
+
+        if node.point.same_as(point):
+            node.point.data.extend(point.data)
+            return node
+
+        node.is_leaf = False
 
         x_axis = rec.center().x < point.x  # apo ayto ews to epomeno comment mporei na ginei
         y_axis = rec.center().y > point.y
@@ -115,9 +132,11 @@ class QuadTree:
                 node.directions[i] = Node(True, rectangles[i], node, None)
 
         if index != index2:
+
             node.directions[index] = Node(True, rectangles[index], node, point)
             node.directions[index2] = Node(True, rectangles[index2], node, node.point)
         else:
+
             temp = Node(False, rectangles[index], node, node.point)
             node.directions[index] = temp
             self.split_node(node.directions[index], rectangles[index], point)
@@ -127,28 +146,65 @@ class QuadTree:
     def search(self, node: Node, rec: Rectangle, point: Point):
         x_axis = rec.center().x < point.x
         y_axis = rec.center().y > point.y
+
         index = 2 * int(x_axis) + int(y_axis)
 
-        if node.is_leaf() and node.is_root():
-            return node
+        if node.isleaf() and node.is_root:
 
-        if node.directions[index].is_leaf():
-            return node.directions[index]
+            return node.point.data
+
+        if node.directions[index].isleaf():
+
+            return node.directions[index].point.data
         else:
+
             return self.search(node.directions[index], node.directions[index].rec, point)
+
+    def query(self, rec: Rectangle, point: Point):
+        a = []
+        for i in range(rec.low.x, rec.high.x+1):
+            for j in range(rec.low.y, rec.high.y+1):
+                a.append(self.search(self.root, Point(i, j)))
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    low_point = Point(65, 0)
-    high_point = Point(90, 12)
+    #
+    df = pd.read_csv("data.txt", sep=" ", header=None)
 
-    point_list = [Point(75, 3), Point(67, 5), Point(70, 10), Point(71, 5), Point(74, 9), Point(80, 3)]
-
+    Xmax = ord(df[0].max())
+    Xmin = ord(df[0].min())
+    Ymax = df[1].max()
+    Ymin = df[1].min()
+    low_point = Point(Xmin, Ymin)
+    high_point = Point(Xmax, Ymax)
     rect = Rectangle(low_point, high_point)
+
     qt = QuadTree(rect)
 
-    for ad in range(len(point_list)):
-        qt.insert(point_list[ad])
+    for pd in range(len(df)):
+        p = Point(ord(df[0][pd]),  df[1][pd], pd)
+        print(f"pd = {pd}")
+        qt.insert(p)
+
+    print(qt.root.directions[1].directions[0].point.data)
+    fl = qt.search(qt.root, rect, Point(ord('a'), 2))
+    print(fl)
+
+    # low_point = Point(65, 0)
+    # high_point = Point(90, 12)
+    #
+    # point_list = [Point(75, 3), Point(67, 5), Point(70, 10), Point(71, 5), Point(74, 9), Point(80, 3)]
+    #
+    # rect = Rectangle(low_point, high_point)
+    # qt = QuadTree(rect)
+    #
+    # for ad in range(len(point_list)):
+    #     qt.insert(point_list[ad])
+
+    # print(f"{len(a)} matches were found :")
+    # print(a)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
