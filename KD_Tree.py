3207018cd0be843ff0a87844b6cb1989b8967e0b
Nikolaos_Voulgaris <83508203+NickVoulg02@users.noreply.github.com>
2023-09-25 13:50:27 +0300

    Update KD_Tree.py

--- KD_Tree.py
+++ KD_Tree.py
@@ -17,43 +17,46 @@ class KD_Tree:
     #BUILDING 2D KD TREE
     def build_kdtree(self, node_list, depth=0):
         n = len(node_list)
-        if n <= 0:
+        
+        if n <= 0:                                                               # empty set
             return None
         
         axis = depth % 2                                                         # Calculating current axis of comparison
         sorted_list = []
         sorted_list = sorted(node_list, key=lambda node: node.point[axis])       # Sorting nodes by axis-value of each point
 
-        node = sorted_list[n//2]                                                 # The spliting point will be the median 
-        if n == 1:
+        node = sorted_list[n//2]                                                 # The spliting point will be the median
+
+        if n == 1:                                                               # We stop spliting when we reach a leaf node
             node.isLeaf = True
-        node.left = self.build_kdtree(sorted_list[:n // 2], depth+1)
-        node.right = self.build_kdtree(sorted_list[n // 2 + 1:], depth+1)
+        else:
+            node.left = self.build_kdtree(sorted_list[:n // 2], depth+1)
+            node.right = self.build_kdtree(sorted_list[n // 2 + 1:], depth+1)
 
         return node
 
 
     #PRINTING FUNCTION
-    def print_kdtree(self, root, n=0):
+    def print_kdtree(self, root, n=1):
         if root is None:
             return
         
-        print(n,". Point is:", root.point, root.isLeaf)
+        print("The node's point is:", root.point)
         if root.left != None:
-            print(n,". Left Subtree")
+            print("Left Child",n)
             self.print_kdtree(root.left,n+1)
         if root.right != None:
-            print(n,". Right Subtree")
+            print("Right Child",n)
             self.print_kdtree(root.right, n+1)
 
 
     # 1D RANGE SEARCH FUNCTION
     def withinRange(self, point, range):
         """"
-        Checks if the value of node is within the required range
+        Checks if the value of the node is within the required range
         Arguments:
-            point       : A point in tree
-            range       : A list containing range to be checked with
+            point       : A point in the tree
+            range       : A two dimensional list containing range of numbers to be checked
         """
 
         x = point[0]
@@ -68,10 +71,10 @@ class KD_Tree:
         Performs 2D range search
         Arguments:
             root        : The tree's root
-            x1          : Starting range for x-coord
-            x2          : Ending range for x-coord
-            y1          : Starting range for y-coord
-            y2          : Ending range for y-coord
+            x1          : Minimum value for x-coord
+            x2          : Maximum value for x-coord
+            y1          : Minimum value for y-coord
+            y2          : Maximum value for y-coord
         '''
         nodes = []
 
@@ -79,9 +82,9 @@ class KD_Tree:
             return nodes
         elif self.withinRange(root.point, [(x1, x2), (y1, y2)]) :     # Check if the node is a valid node in range
             nodes.append(root.index)
-
-
+        
         axis = depth % 2
+
         if root.point[0]>x2 and axis == 0 :
             nodes += self.SearchKDTree2d(root.left, x1, x2, y1, y2, 1)              # Search for nodes in left subtree
             return nodes
@@ -95,7 +98,7 @@ class KD_Tree:
             nodes += self.SearchKDTree2d(root.right, x1, x2, y1, y2, 0)             
             return nodes
         else:
-            nodes += self.SearchKDTree2d(root.left, x1, x2, y1, y2, depth+1)          # Search for nodes in both subtrees
+            nodes += self.SearchKDTree2d(root.left, x1, x2, y1, y2, depth+1)        # Search for nodes in both subtrees
             nodes += self.SearchKDTree2d(root.right, x1, x2, y1, y2, depth+1)        
             
             return nodes 
